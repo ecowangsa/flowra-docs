5fcf229871aa0feb0a1f40089dd22382c4d54898
ecowangsa <ekodahni@gmail.com>
2025-11-14 13:59:35 +0700

    first

---
+++ docusaurus.config.ts
@@ -0,0 +1,113 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Flowra Documentation',
+  tagline: 'A modular, expressive Node.js framework for building scalable web applications - featuring CLI-driven architecture, service container, and seamless module integration.',
+  favicon: 'img/favicon.ico',
+  future: {
+    v4: true,
+  },
+  url: 'https://your-docusaurus-site.example.com',
+  baseUrl: '/',
+  organizationName: 'ecowangsa',
+  projectName: 'flowra-docs',
+
+  onBrokenLinks: 'throw',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: '1.0.0',
+            }
+          },
+          routeBasePath: 'docs',
+          sidebarPath: './sidebars.ts',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Flowra',
+      logo: {
+        alt: 'Flowra Logo',
+        src: 'img/flowra.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          type: 'docsVersionDropdown',
+          versions: {
+            current: {
+              label: '1.0.0',
+            },
+          },
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/getting-started/installation',
+            },
+          ],
+        },
+        {
+          title: 'Social Media',
+          items: [
+            {
+              label: 'Instagram',
+              href: 'https://www.instagram.com/flowra.js/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Flowra, Inc.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
